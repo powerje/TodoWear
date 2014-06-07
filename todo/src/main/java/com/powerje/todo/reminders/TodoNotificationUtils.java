11c87ef31077bbfc2fafc4d7bc630b04012f9158
James Power <heathen79@gmail.com>
2014-06-07 16:38:14 -0400

    Showing all todo items

--- todo/src/main/java/com/powerje/todo/reminders/TodoNotificationUtils.java
+++ todo/src/main/java/com/powerje/todo/reminders/TodoNotificationUtils.java
@@ -26,35 +26,48 @@ public class TodoNotificationUtils {
     public static void setupNotifications(List<TodoNotification> todos, Context context) {
 
         // Nuke all previous notifications and generate unique ids
-        NotificationManagerCompat.from(context).cancelAll();
+//        NotificationManagerCompat.from(context).cancelAll();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         int notificationId = 0;
 
         // String to represent the group all the notifications will be a part of
         final String GROUP_KEY_TODO = "group_key_todo";
+
 
         // Group notification that will be visible on the phone
         NotificationCompat.Builder builderG = new NotificationCompat.Builder(context)
                 .setContentTitle(todos.size() + " todos")
                 .setContentText(todos.get(0).getText())
                 .setSmallIcon(R.drawable.ic_launcher);
+
+        NotificationCompat.InboxStyle style = new NotificationCompat.InboxStyle(builderG)
+                .setBigContentTitle(todos.size() + " todo items");
+        for (TodoNotification n : todos) {
+            style.addLine(n.getText());
+        }
+
         Notification summaryNotification = new WearableNotifications.Builder(builderG)
                 .setGroup(GROUP_KEY_TODO, WearableNotifications.GROUP_ORDER_SUMMARY)
                 .build();
+        notificationManager.notify(notificationId + 0, summaryNotification);
 
-        // Separate notifications that will be visible on the watch
-        Intent viewIntent1 = new Intent(context, MainActivity.class);
-        PendingIntent viewPendingIntent1 =
-                PendingIntent.getActivity(context, notificationId + 1, viewIntent1, 0);
-        NotificationCompat.Builder builder1 = new NotificationCompat.Builder(context)
-                .addAction(R.drawable.ic_launcher, "Treat Fed", viewPendingIntent1)
-                .setContentTitle("Message from Mila")
-                .setContentText("What's for dinner? "
-                        + "Can we have steak?")
-                .setSmallIcon(R.drawable.ic_launcher);
-        Notification notification1 = new WearableNotifications.Builder(builder1)
-                .setGroup(GROUP_KEY_TODO)
-                .build();
+        for (TodoNotification n : todos) {
+            // Separate notifications that will be visible on the watch
+            Intent viewIntent1 = new Intent(context, MainActivity.class);
+            PendingIntent viewPendingIntent1 =
+                    PendingIntent.getActivity(context, notificationId + 1, viewIntent1, 0);
+            NotificationCompat.Builder builder1 = new NotificationCompat.Builder(context)
+                    .addAction(R.drawable.ic_launcher, "Done", viewPendingIntent1)
+                    .setContentTitle("Todo")
+                    .setContentText(n.getText())
+                    .setSmallIcon(R.drawable.ic_launcher);
+            Notification notification1 = new WearableNotifications.Builder(builder1)
+                    .setGroup(GROUP_KEY_TODO)
+                    .build();
+            notificationManager.notify(notificationId++, notification1);
+        }
 
+        /*
         Intent viewIntent2 = new Intent(context, MainActivity.class);
         PendingIntent viewPendingIntent2 =
                 PendingIntent.getActivity(context, notificationId + 2, viewIntent2, 0);
@@ -66,14 +79,15 @@ public class TodoNotificationUtils {
         Notification notification2 = new WearableNotifications.Builder(builder2)
                 .setGroup(GROUP_KEY_TODO)
                 .build();
-
+        */
 // Issue the group notification
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        notificationManager.notify(notificationId + 0, summaryNotification);
+
 
 // Issue the separate wear notifications
+        /*
         notificationManager.notify(notificationId + 2, notification2);
         notificationManager.notify(notificationId + 1, notification1);
+        */
 
 
 //        if (todos.size() == 0) { return; }
