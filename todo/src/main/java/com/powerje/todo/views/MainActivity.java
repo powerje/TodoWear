5a4f0f34f6888a727a929f1139ab612e3db7fd3f
James Power <heathen79@gmail.com>
2014-06-07 13:39:02 -0400

    Hard coded some example todos

--- todo/src/main/java/com/powerje/todo/views/MainActivity.java
+++ todo/src/main/java/com/powerje/todo/views/MainActivity.java
@@ -7,6 +7,11 @@ import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.powerje.todo.R;
+import com.powerje.todo.models.Todo;
+import com.powerje.todo.views.adapters.TodoAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -16,11 +21,18 @@ public class MainActivity extends Activity {
 
     @InjectView(R.id.list) ListView list;
 
+    List<Todo> todos = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
+
+        todos.add(new Todo("Eat pizza"));
+        todos.add(new Todo("Acquire WiFi"));
+        todos.add(new Todo("Make feature branch"));
+        list.setAdapter(new TodoAdapter(todos));
     }
 
     @Override
